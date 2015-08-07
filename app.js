872f02d962fd1be22475e65ee6f927ce0e60ba98
Javier Alvarado <javier_alvarado_m@yahoo.com.mx>
2015-08-07 11:38:39 -0500

    Pregunta con diseño adaptable

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var partials = require('express-partials');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -13,10 +14,12 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
- 
+
+
 // uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
 //app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(partials());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
