2e67135245aa3dd755cca61c8cb369dcea4eb7d1
Javier Alvarado <javier_alvarado_m@yahoo.com.mx>
2015-08-14 15:57:27 -0500

    modulo7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,10 +1,37 @@
 'use strict';
-var models = require('../models/models');
+var models = require('../models/models.js ');
+//Autoload
+exports.load = function)req, res, next, quizId){
+    models.Quiz.find(quizId).then(
+        function(quiz) {
+            if (quiz) {
+                req.quiz = quiz;
+                next();
+            } else{
+                nex (new Error('No existe quizId='+ quizId));
+            }
+        }
+    ).catch(function(error){next(error);});
+};
 //Get /quizes/question
-exports.show = function (req, res){
-    models.Quiz.findAll().succes(function(quiz){
-    res.render('quizes/show', {quiz: quiz});
-    })
+exports.index = function (req, res){
+    models.Quiz.findAll().then(
+        function(quizes){
+    res.render('quizes/index', {quizes: quizes});
+}).catch(function(error) { next(error);})
+};
+//get /quisez/:id
+exports.show = function(req, res) {
+    res.render('quizes/show', {quiz: req.quiz});
+};
+
+//get quizes/id/ansewer
+exports.answer = function(req, res) {
+    var resultado = 'Incorrecto';
+    if (req.query.respuesta === req.quiz.respuesta) {
+        resultado = 'Correcto';
+    }
+    res.render('quisez/answer', {quiz: req.quiz, respuesta: resultado});
 };
 
 //GET /quizes/answer
@@ -17,8 +44,10 @@ exports.show = function (req, res){
     }
     })
 };*/
+/*
 exports.index = function(req, res) {
     models.Quiz.findAll().then(function(quizes) {
         res.render('quizes/index.ejs', {quizes: quizes});
     })
 };
+*/
